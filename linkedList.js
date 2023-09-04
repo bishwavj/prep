d34dbba0e08b47e881baf17627b470bc3e5030fc
Bishwa Vijay Kumar <54392064+bishwavj@users.noreply.github.com>
2023-09-04 20:06:48 +0530

    remove method

--- linkedList.js
+++ linkedList.js
@@ -53,13 +53,45 @@ class LinkedList {
           newNode.next = node2;
           this.size++;
         }
-  };
+  }
+
+  remove(data){
+    if(!this.head)
+    return;
+    
+    let current = this.head;
+    if(current.head === data){
+      this.head = current.next;
+      this.size--;
+      return;
+    }
+
+    
+    let prev = null;
+
+    while(current){
+      if(current.head === data){
+        if(!current.next){ //for removing last element(tail)
+          this.tail = prev;
+          this.tail.next = null;
+        }
+        else
+        prev.next = current.next;
+        this.size--;
+      }
+      prev = current;
+      current = current.next;
+    }
+
+  }
 }
 
 const linkedList = new LinkedList();
 linkedList.add(150);
 linkedList.add(200);
 linkedList.add(300);
-linkedList.addInBtw(750, 150);
+linkedList.add(750);
+linkedList.remove(300);
+// linkedList.addInBtw(750, 150);
 
 console.log("linkedList", linkedList);
